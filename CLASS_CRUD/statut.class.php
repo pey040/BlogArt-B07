9fa51a87dc3876dc12f6003419f82c2bd6510fa8
pey040 <40pey0@gmail.com>
2022-02-14 16:20:26 +0100

    premier push

---
+++ CLASS_CRUD/statut.class.php
@@ -0,0 +1,93 @@
+<?php
+// CRUD STATUT
+// ETUD
+require_once __DIR__ . '../../CONNECT/database.php';
+
+class STATUT
+{
+	function get_1Statut($idStat)
+	{
+		global $db;
+
+		$query = 'SELECT * FROM STATUT WHERE idStat=?;';
+		$request = $db->prepare($query);
+		$request->execute([$idStat]);
+		return ($request->fetch());
+	}
+
+	function get_AllStatuts()
+	{
+		global $db;
+
+		$query = 'SELECT * FROM STATUT;';
+		$result = $db->query($query);
+		$allStatuts = $result->fetchAll();
+		return ($allStatuts);
+	}
+
+	function create($libStat)
+	{
+		global $db;
+
+		try {
+			$db->beginTransaction();
+
+			$query = 'INSERT INTO STATUT (libStat) VALUES (?)';
+			$request = $db->prepare($query);
+			$request->execute([$libStat]);
+			$db->commit();
+			$request->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur insert STATUT : ' . $e->getMessage());
+		}
+	}
+
+	function update($idStat, $libStat)
+	{
+		global $db;
+
+		try {
+			$db->beginTransaction();
+
+			$query = 'UPDATE statut set libStat=? where idStat=?;';
+			$request = $db->prepare($query);
+			$request->execute([$libStat, $idStat]);
+			// update
+			// prepare
+			// execute
+			$db->commit();
+			$request->closeCursor();
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur update STATUT : ' . $e->getMessage());
+		}
+	}
+
+	function delete($idStat)
+	{
+		global $db;
+
+		try {
+			$db->beginTransaction();
+
+			
+			$query = 'DELETE FROM statut where idStat=?;';
+			$request = $db->prepare($query);
+			$request->execute([$idStat]);
+			// delete
+			// prepare
+			// execute
+			$count = $request->rowCount(); //
+			$db->commit();
+			$request->closeCursor();
+			return ($count); //
+		} catch (PDOException $e) {
+			$db->rollBack();
+			$request->closeCursor();
+			die('Erreur delete STATUT : ' . $e->getMessage());
+		}
+	}
+}	// End of class
---
+++ back/statut/deleteStatut.php
@@ -0,0 +1,156 @@
+<?php
+////////////////////////////////////////////////////////////
+//
+//  CRUD STATUT (PDO) - Modifié : 4 Juillet 2021
+//
+//  Script  : deleteStatut.php  -  (ETUD)  BLOGART22
+//
+////////////////////////////////////////////////////////////
+
+// Mode DEV
+require_once __DIR__ . '/../../util/utilErrOn.php';
+
+// controle des saisies du formulaire
+require_once __DIR__ . '/../../util/ctrlSaisies.php';
+// Del accents sur string
+require_once __DIR__ . '/../../util/delAccents.php';
+
+// Insertion classe Statut
+require_once __DIR__ . '/../../CLASS_CRUD/statut.class.php';
+$monStatut = new STATUT();
+// Instanciation de la classe Statut
+
+
+
+
+// Ctrl CIR
+// Insertion classe User
+
+// Instanciation de la classe User
+
+
+// Insertion classe Membre
+
+// Instanciation de la classe Membre
+
+
+// Gestion des erreurs de saisie
+$erreur = false;
+
+// Gestion du $_SERVER["REQUEST_METHOD"] => En POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // controle des saisies du formulaire
+
+    // modification effective du statut
+
+    if (isset($_POST['Submit'])) {
+        $Submit = $_POST['Submit'];
+    } else {
+        $Submit = "";
+    }
+    if ((isset($_POST["Submit"])) and ($Submit === "Initialiser")) {
+
+        header("Location: ./createStatut.php");
+    }   // End of if ((isset($_POST["submit"])) ...
+    if (((isset($_POST['id'])) and !empty($_POST['id']))
+        and (!empty($_POST['Submit']) and ($Submit === "Valider"))
+    ) {
+        // Saisies valides
+        $erreur = false;
+
+        $libStat = ctrlSaisies(($_POST['id']));
+        $monStatut->delete($libStat);
+
+        header("Location: ./statut.php");
+    }   // Fin if ((isset($_POST['libStat'])) ...
+    else {
+        // Saisies invalides
+        $erreur = true;
+        $errSaisies =  "Erreur, la saisie est obligatoire !";
+    }   // End of else erreur saisies
+
+
+    // Gestion des erreurs => msg si saisies ko
+
+
+}   // End of if ($_SERVER["REQUEST_METHOD"] === "POST")
+// Init variables form
+include __DIR__ . '/initStatut.php';
+?>
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="utf-8" />
+    <title>Admin - CRUD Statut</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <style type="text/css">
+        #p1 {
+            max-width: 600px;
+            width: 600px;
+            max-height: 200px;
+            height: 200px;
+            border: 1px solid #000000;
+            background-color: whitesmoke;
+            /* Coins arrondis et couleur du cadre */
+            border: 2px solid grey;
+            -moz-border-radius: 8px;
+            -webkit-border-radius: 8px;
+            border-radius: 8px;
+        }
+        .error {
+            padding: 2px;
+            border: solid 0px black;
+            color: red;
+            font-style: italic;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h1>BLOGART22 Admin - CRUD Statut</h1>
+    <h2>Suppression d'un statut</h2>
+<?php
+    // Supp : récup id à supprimer
+    // id passé en GET
+
+
+
+
+?>
+    <form method="POST" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" accept-charset="UTF-8">
+
+      <fieldset>
+        <legend class="legend1">Formulaire Statut...</legend>
+
+        <input type="hidden" id="id" name="id" value="<?= isset($_GET['id']) ? $_GET['id'] : '' ?>" />
+
+        <div class="control-group">
+            <label class="control-label" for="libStat"><b>Nom :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
+            <input type="text" name="libStat" id="libStat" size="80" maxlength="80" value="<?= $libStat; ?>" disabled="disabled" />
+        </div>
+
+        <div class="control-group">
+            <div class="controls">
+                <br><br>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="submit" value="Annuler" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input type="submit" value="Valider" style="cursor:pointer; padding:5px 20px; background-color:lightsteelblue; border:dotted 2px grey; border-radius:5px;" name="Submit" />
+                <br>
+            </div>
+        </div>
+      </fieldset>
+    </form>
+    <br>
+    <i><div class="error"><br>=>&nbsp;Attention, une suppression doit respecter les CIR !</div></i>
+<?php
+require_once __DIR__ . '/footerStatut.php';
+
+require_once __DIR__ . '/footer.php';
+?>
+</body>
+</html>
